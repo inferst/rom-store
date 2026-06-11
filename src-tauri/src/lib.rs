52b191bf4ee221b753ae18131a808db27c743ed8
Mikhail Morozov <inferst@gmail.com>
2026-06-11 22:35:57 +0300

    feat: github workflow

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,10 +11,19 @@ fn close_app(app: tauri::AppHandle) {
     app.exit(0);
 }
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _, _| {
+            app.webview_windows()
+                .values()
+                .next()
+                .expect("no window found")
+                .set_focus()
+                .expect("failed to set focus");
+        }))
         .invoke_handler(tauri::generate_handler![greet, close_app])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
